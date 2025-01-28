fe4641b47cb565ccf4aa30e7aef09f3240974f51
CleyhanRb <cleyhan.robasse@gmail.com>
2025-01-28 17:41:11 +0100

    ConsoleProgressBar Done.

---
+++ ConsoleProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCypherDecoder_CS
+{
+    internal class ConsoleProgressBar
+    {
+        public int Maximum { get; set; }
+        public int Value { get; set; }
+
+        public ConsoleProgressBar()
+        {
+            this.Maximum = 100;
+        }
+
+        public ConsoleProgressBar(int max)
+        {
+            this.Maximum = max;
+        }
+
+        public string renderBar()
+        {
+            int progress = (int)Math.Floor((decimal)(20 * this.Value / this.Maximum));
+            return $"[{new string('#', progress) + new string('-', 20 - progress)}]";
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CesarCypherDecoder_CS;
+
+
+namespace CesarCypherDecoder_CS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleProgressBar bar = new ConsoleProgressBar(100);
+
+            
+        }
+    }
+}
